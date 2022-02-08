d9ac02f7314959fa566697cfd95e39a196e61f4a
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-08 11:17:33 +0900

    Firebase 연결하기, viewBinding, Splitties

--- app/src/main/java/com/example/testinsta/LoginActivity.kt
+++ app/src/main/java/com/example/testinsta/LoginActivity.kt
@@ -1,11 +1,67 @@
 package com.example.testinsta
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testinsta.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import splitties.activities.start
+import splitties.toast.toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.emailLoginButton.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            binding.emailEdittext.text.toString(),
+            binding.passwordEdittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Creating a user account
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    // Show the error message
+                    toast(task.exception?.message.toString())
+                } else {
+                    // Login if you have accont
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(
+            binding.emailEdittext.text.toString(),
+            binding.passwordEdittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Login
+                } else {
+                    // Login if you have account
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            start<MainActivity>()
+        }
     }
 }
