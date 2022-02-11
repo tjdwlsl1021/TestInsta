702b21df7723515d5eb82e13275f69b33351deb4
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-11 10:19:44 +0900

    알람 리스트 페이지 만들기

--- app/src/main/java/com/example/testinsta/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/testinsta/navigation/AlarmFragment.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.testinsta.R
+import com.example.testinsta.navigation.model.AlarmDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AlarmFragment : Fragment() {
 
@@ -16,6 +23,75 @@ class AlarmFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+
+        val recyclerview = view.findViewById<RecyclerView>(R.id.alarmfragment_recyclerview)
+        recyclerview.adapter = AlarmRecyclerviewAdapter()
+        recyclerview.layoutManager = LinearLayoutManager(activity)
+
         return view
+    }
+
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var alarmDTOList: ArrayList<AlarmDTO> = arrayListOf()
+
+        init {
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+            FirebaseFirestore.getInstance().collection("alarms").whereEqualTo("destinationUid", uid)
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    alarmDTOList.clear()
+                    if (querySnapshot == null) return@addSnapshotListener
+
+                    for (snapshot in querySnapshot.documents) {
+                        alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val view = holder.itemView
+
+            FirebaseFirestore.getInstance().collection("profileImages")
+                .document(alarmDTOList[position].uid!!).get().addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val url = task.result!!["image"]
+                        Glide.with(view.context).load(url).apply(RequestOptions().circleCrop())
+                            .into(view.findViewById(R.id.commentviewitem_imageview_profile))
+                    }
+                }
+
+            when (alarmDTOList[position].kind) {
+                0 -> {
+                    val str_0 =
+                        "${alarmDTOList[position].userId} ${getString(R.string.alarm_favorite)}"
+                    view.findViewById<TextView>(R.id.commentviewitem_textview_profile).text = str_0
+                }
+                1 -> {
+                    val str_0 =
+                        "${alarmDTOList[position].userId} ${getString(R.string.alarm_comment)} of ${alarmDTOList[position].message}"
+                    view.findViewById<TextView>(R.id.commentviewitem_textview_profile).text = str_0
+                }
+                2 -> {
+                    val str_0 =
+                        "${alarmDTOList[position].userId} ${getString(R.string.alarm_follow)}"
+                    view.findViewById<TextView>(R.id.commentviewitem_textview_profile).text = str_0
+                }
+            }
+            view.findViewById<TextView>(R.id.commentviewitem_textview_comment).visibility =
+                View.INVISIBLE
+        }
     }
 }
